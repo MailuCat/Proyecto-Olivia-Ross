f07f3e535194c7eef34c604702a6b5c4dd801811
vicha3663 <f.tramites@gmail.com>
2021-04-17 11:42:09 -0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    $("a").click(function(event) {
+        if (this .hash !== "") {
+
+            event.preventDefault();
+
+            var gato = this .hash;
+
+            $( 'html, body ').animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function() {
+                    window .location.hash = gato;
+                    
+            });
+        }
+    });
+    });
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+  
+ 
+
